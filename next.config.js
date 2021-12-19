7599015f0b5046032a60012cb2465369a184438b
JanineZielman <janine.zielman@icloud.com>
2021-12-19 23:03:01 +0100

    update host link

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@
 const nextConfig = {
   images: {
     loader: "default",
-    domains: ["localhost"],
+    domains: ["localhost", "https://guarded-dusk-98952.herokuapp.com/"],
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
