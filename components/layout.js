d6118795d5e8755bcc2ee30f00cc544253432319
JanineZielman <janine.zielman@icloud.com>
2022-01-13 17:08:17 +0100

    adding content

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,15 @@
 import Nav from "./nav"
 
 const Layout = ({ children, seo }) => (
-  <>
+  <section>
     <Nav/>
     {children}
-  </>
+    <style jsx>{`
+      section{
+        padding: 0 24px;
+      }
+    `}</style>
+  </section>
 )
 
 export default Layout
