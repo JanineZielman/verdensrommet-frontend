59d07e4224a7ab751c52931ffa23802ca1f747c4
JanineZielman <janine.zielman@icloud.com>
2022-04-06 11:49:23 +0200

    update content

--- components/content.js
+++ components/content.js
@@ -7,60 +7,97 @@ const Content = ({ page }) => {
 	console.log(page)
   return (
 	<>
-		{page.content[0]?.background &&
+		{/* {page.section[0]?.settings.background &&
 			<div className="section-bg">
 				<video loop={true} autoPlay="autoPlay" controls muted>
-					<source src={getStrapiMedia(page.content[0].background)} type="video/mp4"/>
+					<source src={getStrapiMedia(page.section[0].settings.background)} type="video/mp4"/>
 					Your browser does not support the video tag.
 				</video>
 			</div>
-		}
+		} */}
 		
-		<section 
-			className="content container"
-			id={page.content[0].section_id}
-			style=
-			{{
-				backgroundColor: page.content[0].background ? "transparent" : "#D5D5D5",
-				color: page.content[0].background ? "#fff" : "#072FDE"
-			}} 
-		>
-			{page.content.map((section, i) => {
+			{page.section.map((item, i) => {
 				return (
-					<div className="wrapper">
-						{section.title && 
-							<h2 key={'title'+i}>{section.title}</h2>
-						}
-						{section.text && 
-							<ReactMarkdown 
-								key={'text'+i}
-								children={section.text} 
-							/>
-						}
-						{section.image && 
-							<Image image={section.image}/>
-						}
-					</div>
+					<section 
+						className="content container"
+						id={item.settings.section_id}
+						style=
+						{{
+							backgroundColor: item.settings.background ? "transparent" : "#D5D5D5",
+							color: item.settings.background ? "#fff" : "#072FDE"
+						}} 
+					>
+						<div className="wrapper">
+							{item.text[0] && 
+							<>
+								{item.text.map((content, i) => {
+									return(
+										<>
+											{content.title && 
+												<h2 key={'title'+i}>{content.title}</h2>
+											}
+											{content.text && 
+												<ReactMarkdown 
+													key={'text'+i}
+													children={content.text} 
+												/>
+											}
+										</>
+									)
+								})}
+							</>
+							}
+							{item.image[0] && 
+								<>
+									{item.image.map((content, i) => {
+										<Image image={content.image}/>
+									})}
+								</>
+							}
+							{item.columns[0] && 
+								<>
+									{item.columns.map((content, i) => {
+										return(
+											<div className="columns">
+												<div className="medium">
+													<ReactMarkdown 
+														key={'column-1'+i}
+														children={content.column_1} 
+													/>
+												</div>
+												<div className="medium">
+													<ReactMarkdown 
+														key={'column-2'+i}
+														children={content.column_2} 
+													/>
+												</div>
+											</div>
+										)
+									})}
+								</>
+							}
+						</div>
+						<style jsx>{`
+							section{
+								background-color: #D5D5D5;
+								color: #072FDE;
+								background-size: cover;
+							}
+							/* h2{
+								font-size: 120px;
+								line-height: 1;
+								margin: 0;
+								padding-bottom: 48px;
+								text-transform: uppercase;
+							} */
+							.wrapper{
+								max-width: 900px;
+							}
+						`}</style>
+					</section>
 				)
 			})}
-			<style jsx>{`
-				section{
-					background-color: #D5D5D5;
-					color: #072FDE;
-					background-size: cover;
-				}
-				h2{
-					font-size: 120px;
-					line-height: 1;
-					margin: 0;
-					padding-bottom: 48px;
-					text-transform: uppercase;
-				}
-				.wrapper{
-					max-width: 900px;
-				}
-			`}</style>
-		</section>
+	
 	</>
   )
 }
