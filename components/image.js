83d73ae28f63a4508dd17c49e0f6305fcf20e685
JanineZielman <janine.zielman@icloud.com>
2022-04-07 10:46:23 +0200

    update

--- components/image.js
+++ components/image.js
@@ -1,8 +1,34 @@
+// import { getStrapiMedia } from "../lib/media"
+// import NextImage from "next/image"
+
+// const Image = ({ image, style }) => {
+//   const { url, alternativeText, width, height } = image
+
+//   const loader = () => {
+//     return getStrapiMedia(image)
+//   }
+
+//   return (
+//     <NextImage
+//       loader={loader}
+//       layout="responsive"
+//       width={width}
+//       height={height}
+//       objectFit="contain"
+//       src={getStrapiMedia(image)}
+//       alt={alternativeText || ""}
+//     />
+//   )
+// }
+
+// export default Image
+
 import { getStrapiMedia } from "../lib/media"
 import NextImage from "next/image"
 
-const Image = ({ image, style }) => {
+const Image = ({ image, layout, objectFit  }) => {
   const { url, alternativeText, width, height } = image
+
 
   const loader = () => {
     return getStrapiMedia(image)
@@ -11,14 +37,17 @@ const Image = ({ image, style }) => {
   return (
     <NextImage
       loader={loader}
-      layout="responsive"
+      layout={layout}
       width={width}
       height={height}
-      objectFit="contain"
+      objectFit={objectFit}
       src={getStrapiMedia(image)}
       alt={alternativeText || ""}
+      className="img"
+      unoptimized={true}
     />
   )
 }
 
 export default Image
+
