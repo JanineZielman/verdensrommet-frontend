21631ab84030d3bbe224e77c5a4311459d83e49d
JanineZielman <janine.zielman@icloud.com>
2022-04-08 23:19:36 +0200

    small changes

--- pages/visa-help/index.js
+++ pages/visa-help/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 import Hero from "../../components/hero"
@@ -6,11 +6,18 @@ import Content from "../../components/content"
 import { fetchAPI } from "../../lib/api"
 
 const VisaHelp = ({ page, pages, homepage }) => {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const username = localStorage.getItem("name");
+    setUsername(username)
+  }, []);
+
   return (
 		<>
-			<Hero page={page} homepage={homepage}/>
+			<Hero page={page} homepage={homepage} username={username}/>
 			<Layout pages={pages} homepage={homepage}>
-        {page.section &&
+        {page.section && username &&
           <Content page={page}/>
         }
 			</Layout>
