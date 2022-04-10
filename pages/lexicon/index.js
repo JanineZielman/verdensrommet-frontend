35aafd0fac9f89873a4e8279aa0893093f412da7
JanineZielman <janine.zielman@icloud.com>
2022-04-10 19:29:33 +0200

    redirect

---
+++ pages/lexicon/index.js
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router';
+import React, {useEffect} from 'react';
+
+const Lexicon = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('https://portal.verdensrommet.network/lexicon');
+  }, []);
+  
+
+  return (
+    <></>
+  )
+}
+
+export default Lexicon;
